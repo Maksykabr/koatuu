3feddd6a1dbd29376364199c8798b5fbaef1cfdc
Максим Абрагамович <maksymabr@gmail.com>
2023-05-08 13:02:42 +0300

    new_commit

--- index.js
+++ index.js
@@ -15,35 +15,47 @@ var thirth_prepared_levels = [];
 var prepared_objects = [];
 
 function sort_levels(array, first_array, second_array, thirth_arrray, fourth_array){
-    array.forEach(function(element) {
-        if (element['Другий рівень'] == '') {
-            first_array.push(element);
-          } else if (element['Третій рівень'] == '') {
-            second_array.push(element);
-          } else if (element['Четвертий рівень'] == '') {
-            thirth_arrray.push(element);
-          } else {
-            fourth_array.push(element);
-          };
-    });
+  var element = 0;
+  while (element <= array.length) {
+    object = array[element]
+    try
+    {
+      if (object['Другий рівень'] == '') {
+        first_array.push(object);
+      } else if (object['Третій рівень'] == '') {
+        second_array.push(object);
+      } else if (object['Четвертий рівень'] == '') {
+        thirth_arrray.push(object);
+      } else {
+        fourth_array.push(object);
+      };
+    }
+    catch (err)
+    {
+      console.log(err)
+    }
+    element++;
+  };
 };
 
 
+
 function filter_array_elements(first_array, second_array, level, new_create, sorted_aray ){
-
-  var first_element = 0;
-  while (first_element <= first_array.length){
-    object = first_array[first_element];
-    filteredArr = second_array.filter(obj => obj[`${level} рівень`] === object[`${level} рівень`]);
-
-
-    console.log(filteredArr)
-    first_element++;
-    
-  };
-  
-  
-  console.log('he')
+  var element = 0;
+  while (element <= first_array.length){
+    object = first_array[element];
+    try
+    {
+      sortedArr = second_array.filter(obj => obj[`${level} рівень`] === object[`${level} рівень`]);
+      object[`Масив елементів ${new_create} рівня`] = sortedArr;
+      sorted_aray.push(object);
+    }
+    catch(err)
+    {
+      // console.log(err)
+    };
+    element++;
+    };
   };
 
  
@@ -53,20 +65,23 @@ fs.readFile(file, 'utf-8', function(error, data){
     all_objects = JSON.parse(data);
 
     sort_levels(all_objects, first_sorted_level, second_sorted_level, thirth_sorted_level, fourth_sorted_level);
-    // console.log(fourth_sorted_level)
-    // filter_array_elements(first_sorted_level)
-    // console.log(first_sorted_level)
+
     filter_array_elements(thirth_sorted_level, fourth_sorted_level, "Третій", "четвертого", thirth_prepared_levels);
-    // console.log(thirth_sorted_level)
-    // console.log(thirth_prepared_levels)
-    // filter_array_elements(second_sorted_level, thirth_prepared_levels, "Другий", "третього", second_prepared_levels);
-    // filter_array_elements(first_sorted_level, second_prepared_levels, "Другого", "другого", prepared_objects);
+    filter_array_elements(second_sorted_level, thirth_prepared_levels, "Другий", "третього", second_prepared_levels);
+    filter_array_elements(first_sorted_level, second_prepared_levels, "Перший", "другого", prepared_objects);
 
     fs.writeFile(new_file, JSON.stringify(prepared_objects), function(error){
-      if (error) throw error;
+      try
+      {
+        console.log("Finish");
+      }
+      catch
+      {
+        console.log(error);
+      }
     });
   }
-  catch
+  catch(error)
   {
     console.log(error);
   }
