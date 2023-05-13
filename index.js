38508f8912b43875f5b012e5095638476d2aab69
Maksykabr <95287886+Maksykabr@users.noreply.github.com>
2023-05-13 13:02:21 +0300

    Koatuu project with while cycle

--- index.js
+++ index.js
@@ -1,79 +1,87 @@
 var fs = require('fs');
-
-
+var file = './data_parse/koatuu.json';
+var new_file = './data_parse/new_koatuu.json';
 var first_sorted_level = [];
 var second_sorted_level = [];
 var thirth_sorted_level = [];
 var fourth_sorted_level = [];
-
 var second_prepared_levels = [];
 var thirth_prepared_levels = [];
+var prepared_objects = [];
+function sort_levels(array, first_array, second_array, thirth_arrray, fourth_array){
+  var element = 0;
+  while (element <= array.length) {
+    object = array[element]
+    try
+    {
+      if (object['Другий рівень'] == '') {
+        first_array.push(object);
+      } else if (object['Третій рівень'] == '') {
+        second_array.push(object);
+      } else if (object['Четвертий рівень'] == '') {
+        thirth_arrray.push(object);
+      } else {
+        fourth_array.push(object);
 
-var prepared_data = [];
-
-var read_file = './data_parse/koatuu.json'
-var new_file = './data_parse/new_koatuu.json'
-
-
-
-fs.readFile(read_file, 'utf-8', function(err, data){
-  if (err) throw err;
-  var objects = JSON.parse(data);
-
-  objects.forEach(function(element){
-    if (element['Другий рівень'] == '') {
-      first_sorted_level.push(element);
-    } else if (element['Третій рівень'] == '') {
-      second_sorted_level.push(element);
-    } else if (element['Четвертий рівень'] == '') {
-      thirth_sorted_level.push(element);
-    } else {
-      fourth_sorted_level.push(element);
+      };
+    }
+    catch (err)
+    {
+      console.log(err)
+    }
+    finally
+    {
+      element++;
     };
-  });
+  };
+};
 
-  function filter_array_elements(first_array, second_array, level, new_create, sorted_aray ){-
-    first_array.forEach(function(element){
-      filteredArr = second_array.filter(obj => obj[`${level} рівень`] === element[`${level} рівень`]);
-      element[`Масив елементів ${new_create} рівня` ] = filteredArr;
-      sorted_aray.push(element)
-        
-    });
+function filter_array_elements(first_array, second_array, level, new_create, sorted_aray ){
+  var element = 0;
+  while (element <= first_array.length){
+    object = first_array[element];
+    try
+    {
+      sortedArr = second_array.filter(obj => obj[`${level} рівень`] === object[`${level} рівень`]);
+      object[`Масив елементів ${new_create} рівня`] = sortedArr;
+      sorted_aray.push(object);
+
+    }
+    catch(err)
+    {
+      console.log(err);
+    }
+    finally
+    {
+      element++;
+    };
+    };
   };
 
-  
-  filter_array_elements(thirth_sorted_level, fourth_sorted_level, "Третій", "четвертого", thirth_prepared_levels);
-  filter_array_elements(fourth_sorted_level, thirth_sorted_level, "Другий", "третього", second_prepared_levels);
-  filter_array_elements(first_sorted_level, second_prepared_levels, "Перший", "другого", prepared_data);
-  // console.log(prepared_data)
-  // thirth_sorted_level.forEach(function(element){
-  //   filteredArr = fourth_sorted_level.filter(function(obj){obj['Третій рівень'] === element['Третій рівень']})
-  //   element['Масив елементів четвертого рівня'] = filteredArr;
-  //   two_prepared_levels.push(element);
-  //   console.log(filteredArr)
+ 
+fs.readFile(file, 'utf-8', function(error, data){
+  try
+  {
+    all_objects = JSON.parse(data);
+    sort_levels(all_objects, first_sorted_level, second_sorted_level, thirth_sorted_level, fourth_sorted_level);
+    filter_array_elements(thirth_sorted_level, fourth_sorted_level, "Третій", "четвертого", thirth_prepared_levels);
+    filter_array_elements(second_sorted_level, thirth_prepared_levels, "Другий", "третього", second_prepared_levels);
+    filter_array_elements(first_sorted_level, second_prepared_levels, "Перший", "другого", prepared_objects);
 
-  // });
-
-  // second_sorted_level.forEach(function(element){
-  //   // filteredArr = two_prepared_levels.filter(function(obj){
-  //   //   obj['Другий рівень'] === element['Другий рівень'];
-  //   // });
-  //   filteredArr = two_prepared_levels.filter(obj => obj['Другий рівень'] === element['Другий рівень'])
-  //   element['Масив елементів третього рівня'] = filteredArr;
-  //   three_prepared_levels.push(filteredArr)
-  // });
-  
-  // first_sorted_level.forEach(function(element){
-  //   filteredArr = three_prepared_levels.filter(function(obj){
-  //     obj['Перший рівень'] === element['Перший рівень'];
-  //   });
-  //   element['Масив елементів другого рівня'] = filteredArr;
-  //   prepared_data.push(element);
-  // });
-
-  new_data = JSON.stringify(prepared_data);
-  // console.log(new_data)
-  fs.writeFileSync(new_file, new_data, function(err){
-    if  (err) throw err;
+    fs.writeFile(new_file, JSON.stringify(prepared_objects), function(error){
+      try
+      {
+        console.log("Finish");
+      }
+      catch
+      {
+        console.log(error);
+      }
+      if (error) throw error
+    });
+  }
+  catch(error)
+  {
+    console.log(error);
+  }
   });
-});
